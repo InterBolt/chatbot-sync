3628dc388af03d67d8f19cd034743f3bb96764cc
campbellman <cc13.engineering@gmail.com>
2023-11-29 21:04:42 -0500

    tested. done

--- bots/test/dynamic.ts
+++
@@ -1,9 +0,0 @@
-import type { DynamicBotBuilder } from "../../build-a-bot/types";
-
-const dynamicBot: DynamicBotBuilder = async (fileBot) => {
-  return {
-    // ADD DYNAMIC OR SENSITIVIE PROMPT DATA HERE
-  };
-};
-
-export default dynamicBot;
