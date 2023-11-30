be45a1f1db2d87e5e89619b517b1b7a58790c69f
campbellman <cc13.engineering@gmail.com>
2023-11-29 21:13:28 -0500

    added env example and removed pre-commit for non-maintainers

--- scripts/pre-commit.ts
+++ scripts/pre-commit.ts
@@ -3,6 +3,7 @@ import { readdir } from "fs/promises";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 import log from "./log";
+import dotenv from "dotenv";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -18,4 +19,22 @@ const main = async () => {
   });
 };
 
-main();
+const pathToEnv = resolve(process.cwd(), ".env");
+
+if (!existsSync(pathToEnv)) {
+  log.error(
+    `Missing .env file in the root of the project. If you just cloned the repo, run "mv example-env .env".`
+  );
+  process.exit(1);
+}
+
+const { parsed: ENV } = dotenv.config({
+  path: resolve(process.cwd(), ".env"),
+});
+
+if (ENV?.USER === "maintainer") {
+  main();
+  log.success("Removed deployments for maintainer.");
+} else {
+  log.success("Persisted deployments for non-maintainer.");
+}
